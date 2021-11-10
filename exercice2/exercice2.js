fa0d6cc8b0d1b7676d6ce6a4e2f1413768770ff5
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-10 15:30:01 +0100

    ..

--- exercice2/exercice2.js
+++ exercice2/exercice2.js
@@ -1,9 +1,15 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+let x = 1;
+let y = 2;
 
-
-(x  y) {
+if (x > y) {
     document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
-}  {
+}
+
+if (x < y){
     document.getElementById('monDiv').innerHTML = "La variable x n'est pas supérieure à la variable y";
+}
+else if( x === y ) {
+    alert("X est plus petit que Y")
 }
 
--- exercice3/exercice3.js
+++ exercice3/exercice3.js
@@ -1,10 +1,12 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+let x = 1;
+let y = 2;
 
-if()
-{
-
+if ( !(x === y) === true) {
+    document.getElementById('monDiv').innerHTML = "Ma variable X est strictement égal a Y !";
+}
+else {
+    alert("X est bien égale à Y");
 }
 
-{
 
-}
